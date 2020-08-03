d783213724fe1b710ec93dee362a150d5c53e26c
Ash <valleyknight@protonmail.com>
2020-08-03 02:09:49 -0400

    list both explicitly installed package count and total package count for portage

--- src/info/packages.rs
+++ src/info/packages.rs
@@ -1,9 +1,17 @@
 use glob::glob;
+use std::fs::File;
+use std::io::{BufReader, Read};
 
 pub fn packages(manager: &str) -> String {
     let mut list: Vec<String> = Vec::new();
     match manager {
         "portage" => {
+            let file = File::open("/var/lib/portage/world").unwrap();
+            let mut buf_reader = BufReader::new(file);
+            let mut contents = String::new();
+            buf_reader.read_to_string(&mut contents).unwrap();
+            let file_vector: Vec<&str> = contents.split("\n").collect();
+
             for entry in glob("/var/db/pkg/*/*/").expect("Failed to read glob pattern") {
                 match entry {
                     Ok(path) => list.push(path.display().to_string()),
@@ -11,8 +19,10 @@ pub fn packages(manager: &str) -> String {
                 }
             }
 
-            let amount = list.iter().count();
-            amount.to_string()
+            let explicit_pre = file_vector.iter().count();
+            let explicit = explicit_pre - 1;
+            let total = list.iter().count();
+            format!("{} (explicit), {} (total)", explicit, total)
         }
         _ => format!("N/A ({} is not supported)", manager),
     }
