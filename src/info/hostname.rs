ee2eb10baa5745ce82960107dd9f1c55911bd9b1
Ash <valleyknight@protonmail.com>
2020-08-02 23:39:35 -0400

    replace unnecessary expects with unwraps

--- src/info/hostname.rs
+++ src/info/hostname.rs
@@ -3,7 +3,7 @@ use std::fs::{metadata, read_to_string};
 
 pub fn hostname() -> String {
     if metadata("/etc/hostname").is_ok() {
-        read_to_string("/etc/hostname").expect("Could not read file for hostname.")
+        read_to_string("/etc/hostname").unwrap()
     } else {
         error!("Could not obtain hostname.");
         "N/A".to_string()
--- src/info/kernel.rs
+++ src/info/kernel.rs
@@ -3,8 +3,7 @@ use std::fs::{metadata, read_to_string};
 
 pub fn kernel() -> String {
     if metadata("/proc/sys/kernel/osrelease").is_ok() {
-        read_to_string("/proc/sys/kernel/osrelease")
-            .expect("Could not read file for kernel version.")
+        read_to_string("/proc/sys/kernel/osrelease").unwrap()
     } else {
         error!("Could not obtain kernel version.");
         "N/A".to_string()
