eb7ab1ba8821c7a72de72180147a1f001fb03f26
Ash <valleyknight@protonmail.com>
2020-08-04 00:59:16 -0400

    remove sedregex

--- src/info/env.rs
+++ src/info/env.rs
@@ -1,7 +1,5 @@
-use sedregex::find_and_replace;
 use std::env;
 
 pub fn env(var: String) -> String {
-    let env_var = env::var(var).unwrap();
-    find_and_replace(&env_var, &["s/\"//"]).unwrap().to_string()
+    env::var(var).unwrap()
 }
