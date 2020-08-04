94be6199bb2e04a3804ceaafa42137984a8c1ee4
Ash <valleyknight@protonmail.com>
2020-08-04 01:01:46 -0400

    add expect explaining error for env

--- src/info/env.rs
+++ src/info/env.rs
@@ -1,5 +1,6 @@
 use std::env;
 
 pub fn env(var: String) -> String {
-    env::var(var).unwrap()
+    // $SHELL and $USER are set automatically, the only env variable it would fail on is $EDITOR
+    env::var(var).expect("Could not read $EDITOR, are you sure it's set?")
 }
