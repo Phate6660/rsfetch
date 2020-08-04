8a44bcb0d5621926a8b8cdb204e787a8a3e45b08
Ash <valleyknight@protonmail.com>
2020-08-04 02:27:00 -0400

    remove useless .into_iter()

--- src/info/packages.rs
+++ src/info/packages.rs
@@ -5,7 +5,7 @@ use std::process::{Command, Output};
 
 fn count(output: Output) -> usize {
     let raw_list = String::from_utf8_lossy(&output.stdout);
-    let list: Vec<&str> = raw_list.split('\n').into_iter().collect();
+    let list: Vec<&str> = raw_list.split('\n').collect();
     list.iter().count()
 }
 
