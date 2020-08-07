b55712d3ab3baf297cf2a8d36a461f0284f25097
Ash <valleyknight@protonmail.com>
2020-08-06 22:15:20 -0400

    remove quotes from distro

--- src/async_functions/async_distro.rs
+++ src/async_functions/async_distro.rs
@@ -13,6 +13,6 @@ pub async fn async_distro(matches: &ArgMatches<'_>) {
 #[cfg(feature = "plain_output")]
 pub async fn async_distro(matches: &ArgMatches<'_>) {
     if matches.is_present("distro") {
-        println!("Distro:       {}", distro().trim());
+        println!("Distro:       {}", distro().trim().replace("\"", ""));
     }
 }
